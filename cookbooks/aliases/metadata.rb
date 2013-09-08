865ae2c7bdd77caebddec46b39681e48eb5c6317
Kenny To <sheheitthey@gmail.com>
2013-09-07 17:57:52 -0700

    Run 'knife cookbook create aliases'.

---
+++ cookbooks/aliases/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "sheheitthey"
+maintainer_email "sheheitthey@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures aliases"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
